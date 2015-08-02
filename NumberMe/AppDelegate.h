335f04509549aa80ee1d812ad48c34c46018a104
Nazgugu <prada520farrell@gmail.com>
2015-08-02 12:29:29 -0700

    files

---
+++ NumberMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NumberMe
+//
+//  Created by Liu Zhe on 15/8/2.
+//  Copyright (c) 2015年 Liu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
